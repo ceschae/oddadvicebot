cb25de01cbb6d918c407924bee4b52d738aa3552
Caitlin Schaefer <ceschae@gmail.com>
2019-02-27 16:04:27 -0800

    the grammar sort of makes sense

---
+++ generatetweets.py
@@ -0,0 +1,31 @@
+from random import randint
+import re
+
+def generate(grammar, nt):
+    index = randint(0, len(grammar[nt]) - 1)
+    choice = grammar[nt][index]
+    literal = choice.split(' ')
+    sentence = ''
+    for word in literal:
+        if word.startswith('<'):
+            if not word.endswith('>'):
+                sentence += ' ' + generate(grammar, word[:-1]) + word[-1:]
+            else:
+                sentence += ' ' + generate(grammar, word)
+        else:
+            sentence += ' ' + word
+    return sentence[1:]
+
+grammar = {}
+
+with open('advice.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.replace('\n', '')
+        res = line.split('::=')
+        choices = res[1].split('|')
+        grammar[res[0]] = choices 
+
+howmany = 10
+for i in range(howmany):
+    print(re.sub(r'\s+', ' ', generate(grammar, '<s>')))
